9010fe3f00a75ec6c28385660c29a15f7e427a43
林显佐 <linxz@minstone.com.cn>
2018-01-29 01:10:22 +0800

    提交

--- js/game.js
+++ js/game.js
@@ -26,12 +26,21 @@ var gamedata=[
 [0,0,0,0,0,0,0,0,0,0],
 [0,0,0,0,0,0,0,0,0,0]
 ]
+var nextdivs=[];
+var gamedivs=[];
 //渲染gamedata
 var init=function(){
 	for(var i=0;i<gamedata.length;i++){
-		for(var n=0;n<gamedata[i].length;n++){
-			
+		var gamediv=[];
+		for(var n=0;n<gamedata[0].length;n++){
+			var newNone=document.createElement('div');
+			newNone.className='none';
+			newNone.style.top=(i*20)+'px';
+			newNone.style.left=(n*20)+'px';
+			document.getElementById('game').appendChild(newNone);
+			gamediv.push(newNone);
 		}
+		gamedivs.push(gamediv)
 	}
 }
-
+init()
